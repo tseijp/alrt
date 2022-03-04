2aa27e8bde484ae8f2e987f77f3d9ba2d13bc9d2
tseijp <t1810394@edu.cc.uec.ac.jp>
2022-03-04 10:29:34 +0900

    :bug:fix: remix bug and add theme

---
+++ app/components/Stage.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react'
+import { Canvas } from '@react-three/fiber'
+import type { MeshProps, Props } from '@react-three/fiber'
+import { PresentationControls, ContactShadows, MeshReflectorMaterial } from '@react-three/drei'
+import { useTheme } from './Theme'
+
+const { PI } = Math
+
+
+const Reflector = (props: MeshProps) => (
+  <mesh {...props}>
+    <planeGeometry args={[170, 170]} />
+    {/* @ts-ignore */}
+    <MeshReflectorMaterial
+      blur={[300, 100]}
+      resolution={2048}
+      mixBlur={1}
+      mixStrength={40}
+      roughness={1}
+      depthScale={1.2}
+      minDepthThreshold={0.4}
+      maxDepthThreshold={1.4}
+      color="#101010"
+      metalness={0.5}
+    />
+  </mesh>
+)
+
+export function Stage (props: Props) {
+  const { children, ...other } = props
+  const theme = useTheme()
+  const color = theme.dark? '#101010': '#fff'
+  return (
+    <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4] }} {...other}>
+      <color attach="background" args={[color]} />
+      <fog attach="fog" args={[color, 10, 20]} />
+      <ambientLight intensity={.5} />
+      <spotLight position={10} angle={.15} penumbra={1} shadow-mapSize={[512, 512]} castShadow/>
+      <Reflector position-y={-1} rotation-x={-PI / 2} />
+      <PresentationControls
+        global
+        config={{ mass: 2, tension: 500 }}
+        snap={{ mass: 4, tension: 1500 } as any}
+        rotation-y={0.3}
+        polar={[-PI / 3, PI / 3]}
+        azimuth={[-PI / 1.4, PI / 2]}
+        children={children}
+      />
+      <ContactShadows
+        rotation-x={PI / 2}
+        position={[0, -1.4, 0]}
+        opacity={0.75}
+        width={10}
+        height={10}
+        blur={2.6}
+        far={2}
+      />
+    </Canvas>
+  )
+}
--- app/components/Theme.tsx
+++ app/components/Theme.tsx
@@ -1,5 +1,14 @@
+import * as React from 'react'
 import styled from 'styled-components'
 import { animated } from 'react-spring'
+
+export const ThemeContext = React.createContext<ThemeProps>({})
+export const ThemeProvider = ThemeContext.Provider
+export const useTheme = () => React.useContext(ThemeContext)
+export type ThemeProps = Partial<{
+  dark: boolean
+  mute: boolean
+}>
 
 export interface Theme {
   <Props>(props: Props): null | JSX.Element
@@ -8,8 +17,10 @@ export interface Theme {
   Flex: <Props>(props: Props & FlexProps) => null | JSX.Element
 }
 
-export const Theme = (props: any) => {
-  return props.children
+export const Theme = (props: {theme: ThemeProps, children: React.ReactNode}) => {
+  // return props.children
+  const { theme, ...other } = props
+  return <ThemeProvider value={theme} {...other} />
 }
 
 Theme.Title = styled(animated.h1)`
@@ -18,13 +29,18 @@ Theme.Title = styled(animated.h1)`
 `
 
 Theme.Text = styled(animated.div)`
-  font-size: 2rem;
+  font-size: 4rem;
   width: 100%;
 `
 
 export type FlexProps = Partial<{
+  $w: number|string
+  $h: number|string
+  $p: number|string
+  $m: number|string
   $wrap: boolean
   $row: boolean
+  $red: boolean
   $end: boolean
   $start: boolean
   $gap: string
@@ -32,13 +48,15 @@ export type FlexProps = Partial<{
 
 Theme.Flex = styled.div<FlexProps>`
   display: flex;
-  width: 100%;
-  height: 100%;
-  margin: auto;
   text-align: center;
   align-items: center;
   justify-content: center;
-  ${({$row}) => `flex-direction: ${$row? "row": "column"};`}
   ${({$wrap}) => $wrap && `flex-wrap: wrap;`}
-  ${({$gap}) => $gap && `gap: ${$gap}`}
+  ${({$gap}) => $gap && `gap: ${$gap};`}
+  ${({$red}) => $red && `background: red;`}
+  ${({$row}) => `flex-direction: ${$row? "row": "column"};`}
+  ${({$w}) => `width: ${$w || '100%'};`}
+  ${({$h}) => `height: ${$h || '100%'};`}
+  ${({$m}) => `margin: ${$m || 'auto'};`}
+  ${({$p}) => `padding: ${$p || 'auto'};`}
 `
